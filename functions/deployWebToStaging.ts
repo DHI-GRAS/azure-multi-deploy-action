ab771ac962da1bff431ea4a340b96bab19ea9f00
rira <rira@dhigroup.com>
2021-07-27 13:27:11 +0200

    Test action

--- functions/deployWebToStaging.ts
+++ functions/deployWebToStaging.ts
@@ -1,46 +1,46 @@
-import { exec } from 'child-process-promise'
-import { join } from 'path'
-import { PackageWithName } from '../types'
+// import { exec } from 'child-process-promise'
+// import { join } from 'path'
+// import { PackageWithName } from '../types'
 
-const mdLinebreak = '<br/>'
-const msgFile = join(__dirname, '../../../../', 'github_message.txt')
+// const mdLinebreak = '<br/>'
+// const msgFile = join(__dirname, '../../../../', 'github_message.txt')
 
-export default async (
-	pkg: PackageWithName,
-	pullNumber: number,
-): Promise<void> => {
-	try {
-		if (!pullNumber) throw Error('PR number is undefined')
-		const slotName = pullNumber
-		const stagName = `${pkg.id}stag`
+// export default async (
+// 	pkg: PackageWithName,
+// 	pullNumber: number,
+// ): Promise<void> => {
+// 	try {
+// 		if (!pullNumber) throw Error('PR number is undefined')
+// 		const slotName = pullNumber
+// 		const stagName = `${pkg.id}stag`
 
-		const path = join(__dirname, `${pkg.type}s`, pkg.name)
+// 		const path = join(__dirname, `${pkg.type}s`, pkg.name)
 
-		console.log(`Building webapp: ${pkg.name}`)
-		const { stdout, stderr } = await exec(
-			`cd ${path} && STAG_SLOT=${slotName} yarn ${pkg.name}:build`,
-		)
-		if (stderr) console.log(stderr)
+// 		console.log(`Building webapp: ${pkg.name}`)
+// 		const { stdout, stderr } = await exec(
+// 			`cd ${path} && STAG_SLOT=${slotName} yarn ${pkg.name}:build`,
+// 		)
+// 		if (stderr) console.log(stderr)
 
-		console.log(stdout)
-		console.log(`Build finished, uploading webapp: ${pkg.name}`)
+// 		console.log(stdout)
+// 		console.log(`Build finished, uploading webapp: ${pkg.name}`)
 
-		await exec('az extension add --name storage-preview').catch()
-		const { stdout: uploadOut } = await exec(
-			`cd ${path}/dist/ && az storage azcopy blob upload --container \\$web --account-name ${stagName} --source ./\\* --destination ${slotName} --auth-mode key`,
-		).catch((err) => {
-			throw Error(err)
-		})
-		console.log(uploadOut)
+// 		await exec('az extension add --name storage-preview').catch()
+// 		const { stdout: uploadOut } = await exec(
+// 			`cd ${path}/dist/ && az storage azcopy blob upload --container \\$web --account-name ${stagName} --source ./\\* --destination ${slotName} --auth-mode key`,
+// 		).catch((err) => {
+// 			throw Error(err)
+// 		})
+// 		console.log(uploadOut)
 
-		// Don't think the deployment url gets returned from upload - hopefully this stays static?
-		const deployMsg = `✅ Deployed web app **${pkg.name}** on: https://${stagName}.z16.web.core.windows.net/${slotName} ${mdLinebreak}`
-		await exec(`echo "${deployMsg}" >> ${msgFile}`)
+// 		// Don't think the deployment url gets returned from upload - hopefully this stays static?
+// 		const deployMsg = `✅ Deployed web app **${pkg.name}** on: https://${stagName}.z16.web.core.windows.net/${slotName} ${mdLinebreak}`
+// 		await exec(`echo "${deployMsg}" >> ${msgFile}`)
 
-		console.log(deployMsg)
-	} catch (err) {
-		const deployMsg = `❌ Deployment of web app **${pkg.id}** failed. See CI output for details ${mdLinebreak}`
-		await exec(`echo "${deployMsg}" >> ${msgFile}`)
-		console.log(err)
-	}
-}
+// 		console.log(deployMsg)
+// 	} catch (err) {
+// 		const deployMsg = `❌ Deployment of web app **${pkg.id}** failed. See CI output for details ${mdLinebreak}`
+// 		await exec(`echo "${deployMsg}" >> ${msgFile}`)
+// 		console.log(err)
+// 	}
+// }
--- functions/getChangedPackages.ts
+++ functions/getChangedPackages.ts
@@ -1,87 +1,87 @@
-import { readFileSync } from 'fs'
-import path from 'path'
-import { exec } from 'child-process-promise'
-import { PackagesWithName, PackageWithName } from '../types'
+// import { readFileSync } from 'fs'
+// import path from 'path'
+// import { exec } from 'child-process-promise'
+// import { PackagesWithName, PackageWithName } from '../types'
 
-const packageTypes = ['apps', 'func-apis']
+// const packageTypes = ['apps', 'func-apis']
 
-const deployablePackages: Packages = packageTypes.reduce((acc, pkgType) => {},
-[])
+// const deployablePackages: Packages = packageTypes.reduce((acc, pkgType) => {},
+// [])
 
-interface PackageJSON {
-	dependencies: Record<string, string>
-	devDependencies: Record<string, string>
-}
+// interface PackageJSON {
+// 	dependencies: Record<string, string>
+// 	devDependencies: Record<string, string>
+// }
 
-export default async (): Promise<PackagesWithName> => {
-	try {
-		const packagesWithName = Object.keys(config).map((packageName) => ({
-			name: packageName,
-			...config[packageName],
-		}))
+// export default async (): Promise<PackagesWithName> => {
+// 	try {
+// 		const packagesWithName = Object.keys(config).map((packageName) => ({
+// 			name: packageName,
+// 			...config[packageName],
+// 		}))
 
-		const { stdout: branchName, stderr: branchErr } = await exec(
-			`git branch --show-current`,
-		)
-		if (branchErr) throw Error(branchErr)
+// 		const { stdout: branchName, stderr: branchErr } = await exec(
+// 			`git branch --show-current`,
+// 		)
+// 		if (branchErr) throw Error(branchErr)
 
-		// TODO TEMPORARY while diff checking is dumb
-		const deployablePkgs = packagesWithName.filter(
-			(pkg) => pkg.type === 'app' || pkg.type === 'func-api',
-		)
-		if (branchName.trim() === 'main') return deployablePkgs
+// 		// TODO TEMPORARY while diff checking is dumb
+// 		const deployablePkgs = packagesWithName.filter(
+// 			(pkg) => pkg.type === 'app' || pkg.type === 'func-api',
+// 		)
+// 		if (branchName.trim() === 'main') return deployablePkgs
 
-		const checkChanged = async (pkg: PackageWithName) => {
-			const { stdout: diffOut, stdout: diffErr } = await exec(
-				`git diff --quiet origin/main HEAD -- ${path.join(
-					'..',
-					'..',
-					`${pkg.type}s`,
-					pkg.name,
-				)} || echo changed`,
-			)
-			if (diffErr) console.log(diffErr)
+// 		const checkChanged = async (pkg: PackageWithName) => {
+// 			const { stdout: diffOut, stdout: diffErr } = await exec(
+// 				`git diff --quiet origin/main HEAD -- ${path.join(
+// 					'..',
+// 					'..',
+// 					`${pkg.type}s`,
+// 					pkg.name,
+// 				)} || echo changed`,
+// 			)
+// 			if (diffErr) console.log(diffErr)
 
-			return diffOut.includes('changed') ? pkg : null
-		}
+// 			return diffOut.includes('changed') ? pkg : null
+// 		}
 
-		const changedPromises = packagesWithName.map(checkChanged)
-		const changedDiffPackages = (await Promise.all(changedPromises)).filter(
-			(item) => item,
-		) as PackagesWithName
+// 		const changedPromises = packagesWithName.map(checkChanged)
+// 		const changedDiffPackages = (await Promise.all(changedPromises)).filter(
+// 			(item) => item,
+// 		) as PackagesWithName
 
-		const changedLibPackeges = changedDiffPackages.filter(
-			(pkg) => pkg.type === 'lib',
-		)
+// 		const changedLibPackeges = changedDiffPackages.filter(
+// 			(pkg) => pkg.type === 'lib',
+// 		)
 
-		const libDepPackages = packagesWithName.filter((pkg) => pkg.type === 'app')
-		const changedPackagesWithLibDeps = libDepPackages.filter((pkg) => {
-			const pkgPackageFile = readFileSync(
-				path.join('..', '..', `${pkg.type}s`, pkg.name, 'package.json'),
-				'utf8',
-			)
+// 		const libDepPackages = packagesWithName.filter((pkg) => pkg.type === 'app')
+// 		const changedPackagesWithLibDeps = libDepPackages.filter((pkg) => {
+// 			const pkgPackageFile = readFileSync(
+// 				path.join('..', '..', `${pkg.type}s`, pkg.name, 'package.json'),
+// 				'utf8',
+// 			)
 
-			const pkgPackageJson = JSON.parse(pkgPackageFile) as PackageJSON
-			const pkgDeps = [
-				...Object.keys(pkgPackageJson.dependencies),
-				...Object.keys(pkgPackageJson.devDependencies),
-			]
-			return changedLibPackeges
-				.map((libPkg) => pkgDeps.includes(libPkg.name))
-				.includes(true)
-		})
+// 			const pkgPackageJson = JSON.parse(pkgPackageFile) as PackageJSON
+// 			const pkgDeps = [
+// 				...Object.keys(pkgPackageJson.dependencies),
+// 				...Object.keys(pkgPackageJson.devDependencies),
+// 			]
+// 			return changedLibPackeges
+// 				.map((libPkg) => pkgDeps.includes(libPkg.name))
+// 				.includes(true)
+// 		})
 
-		const changedPackages = [
-			...new Set([...changedPackagesWithLibDeps, ...changedDiffPackages]),
-		] as PackagesWithName
+// 		const changedPackages = [
+// 			...new Set([...changedPackagesWithLibDeps, ...changedDiffPackages]),
+// 		] as PackagesWithName
 
-		const changedPackageIdString = changedPackages
-			.map((pkg) => pkg.id)
-			.join(', ')
-		console.log(`Changed packages: ${changedPackageIdString}`)
+// 		const changedPackageIdString = changedPackages
+// 			.map((pkg) => pkg.id)
+// 			.join(', ')
+// 		console.log(`Changed packages: ${changedPackageIdString}`)
 
-		return changedPackages
-	} catch (err) {
-		throw Error(err)
-	}
-}
+// 		return changedPackages
+// 	} catch (err) {
+// 		throw Error(err)
+// 	}
+// }
