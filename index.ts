4877a4fa44c2474ba27875f9ad3e8ba5951af2e9
rira <rira@dhigroup.com>
2021-07-27 09:08:17 +0200

    Add test

--- index.ts
+++ index.ts
@@ -1,2 +1,6 @@
-import core, { PushEvent } from '@actions/core'
+import core from '@actions/core'
 import github from '@actions/github'
+import { PushEvent } from '@octokit/webhooks-definitions/schema'
+
+const { context } = github
+console.log(context)
