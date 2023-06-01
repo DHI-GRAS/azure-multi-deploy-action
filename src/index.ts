d755e83b95d7a2bd663e06124c2af776a1062954
DenizYil <denizyil@outlook.dk>
2023-06-01 14:10:20 +0200

    try this?

--- src/index.ts
+++ src/index.ts
@@ -23,88 +23,97 @@ const prNumber = payload.pull_request?.number ?? 0
 chalk.level = 1
 
 const run = async () => {
-	const startTime = new Date()
-	const AzureCliInstallSpinner = ora(
-		`${chalk.bold.cyan('1. Installing azure CLI...'.toUpperCase())}`,
-	).start()
-
-	await exec('curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash')
-	AzureCliInstallSpinner.succeed()
-
-	// Use the below version in case specific version has to be installed
-	// await exec(`
-	// 	sudo apt-get update ;
-
-	// 	sudo apt-get install ca-certificates curl apt-transport-https lsb-release gnupg ;
-
-	// 	curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null ;
-
-	// 	AZ_REPO=$(lsb_release -cs) ;
-
-	// 	echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | sudo tee /etc/apt/sources.list.d/azure-cli.list ;
-
-	// 	sudo apt-get update ;
-	// 	sudo apt-get install azure-cli=2.28.0-1~focal --allow-downgrades
-
-	// `)
-	const AzureCliLoginSpinner = ora(
-		`${chalk.bold.cyan('2. Logging into Azure CLI...'.toUpperCase())}`,
-	).start()
-	await azLogin()
-	AzureCliLoginSpinner.succeed()
-
-	if (payload.pull_request?.state !== 'closed') {
-		const CreatingServicesSpinner = ora(
-			`${chalk.bold.cyan('3. Creating missing services...'.toUpperCase())}`,
+	try {
+		const startTime = new Date()
+		const AzureCliInstallSpinner = ora(
+			`${chalk.bold.cyan('1. Installing azure CLI...'.toUpperCase())}`,
 		).start()
-		await createServices(prNumber)
-		CreatingServicesSpinner.succeed()
-	}
-	// Deploy to stag
-	if (isPR && payload.pull_request?.state === 'open') {
-		console.log('\n')
+
+		await exec('curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash')
+		AzureCliInstallSpinner.succeed()
+
+		// Use the below version in case specific version has to be installed
+		// await exec(`
+		// 	sudo apt-get update ;
+
+		// 	sudo apt-get install ca-certificates curl apt-transport-https lsb-release gnupg ;
+
+		// 	curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null ;
+
+		// 	AZ_REPO=$(lsb_release -cs) ;
+
+		// 	echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | sudo tee /etc/apt/sources.list.d/azure-cli.list ;
+
+		// 	sudo apt-get update ;
+		// 	sudo apt-get install azure-cli=2.28.0-1~focal --allow-downgrades
+
+		// `)
+		const AzureCliLoginSpinner = ora(
+			`${chalk.bold.cyan('2. Logging into Azure CLI...'.toUpperCase())}`,
+		).start()
+		await azLogin()
+		AzureCliLoginSpinner.succeed()
+
+		if (payload.pull_request?.state !== 'closed') {
+			const CreatingServicesSpinner = ora(
+				`${chalk.bold.cyan('3. Creating missing services...'.toUpperCase())}`,
+			).start()
+			await createServices(prNumber)
+			CreatingServicesSpinner.succeed()
+		}
+		// Deploy to stag
+		if (isPR && payload.pull_request?.state === 'open') {
+			console.log('\n')
+			console.log(
+				`${chalk.bold.cyan('4. Deploying to staging...'.toUpperCase())}`,
+			)
+			await deployToStag(prNumber)
+		}
+
+		// Deploy to prod
+		const preventProdDeploy = core.getInput('preventProdDeploy')
+		if (preventProdDeploy && currentBranch === defaultBranch) {
+			const errorMsg = `${chalk.bold.yellow(
+				'Info',
+			)}: Production deployment skipped! Code quality checks have failed`
+			console.error(errorMsg)
+			core.setFailed(errorMsg)
+		}
+
+		if (
+			!payload.pull_request &&
+			currentBranch === defaultBranch &&
+			!preventProdDeploy
+		) {
+			console.log('\n')
+			console.log(
+				`${chalk.bold.cyan('4. Deploying to production...').toUpperCase()}`,
+			)
+			await deployToProd()
+		}
+
+		if (isPR && payload.pull_request?.state === 'closed') {
+			console.log(
+				`${chalk.bold
+					.cyan('3. PR closed. Cleaning up deployments...')
+					.toUpperCase()}`,
+			)
+			await cleanDeployments(prNumber)
+		}
+
 		console.log(
-			`${chalk.bold.cyan('4. Deploying to staging...'.toUpperCase())}`,
+			`${chalk.bold.green('Success')}: You are lucky, the action finished! 🍀`,
 		)
-		await deployToStag(prNumber)
+
+		if (isPR) {
+			await postComment(startTime)
+		}
+	} catch (error) {
+		console.log('Global try/catch error', error)
+
+		const message: string = error.message || JSON.stringify(error)
+		core.setFailed(`Action failed, error: ${message}`)
 	}
-
-	// Deploy to prod
-	const preventProdDeploy = core.getInput('preventProdDeploy')
-	if (preventProdDeploy && currentBranch === defaultBranch) {
-		const errorMsg = `${chalk.bold.yellow(
-			'Info',
-		)}: Production deployment skipped! Code quality checks have failed`
-		console.error(errorMsg)
-		core.setFailed(errorMsg)
-	}
-
-	if (
-		!payload.pull_request &&
-		currentBranch === defaultBranch &&
-		!preventProdDeploy
-	) {
-		console.log('\n')
-		console.log(
-			`${chalk.bold.cyan('4. Deploying to production...').toUpperCase()}`,
-		)
-		await deployToProd()
-	}
-
-	if (isPR && payload.pull_request?.state === 'closed') {
-		console.log(
-			`${chalk.bold
-				.cyan('3. PR closed. Cleaning up deployments...')
-				.toUpperCase()}`,
-		)
-		await cleanDeployments(prNumber)
-	}
-
-	console.log(
-		`${chalk.bold.green('Success')}: You are lucky, the action finished! 🍀`,
-	)
-
-	if (isPR) await postComment(startTime)
 }
 
 void run().catch((err) => {
